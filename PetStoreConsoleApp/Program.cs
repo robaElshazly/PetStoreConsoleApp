e3806f8b1000686a8b080085c158126bb1149597
Roba <roba@github.com>
2022-04-08 00:14:49 +1000

    Handling request exceptions

--- PetStoreConsoleApp/Program.cs
+++ PetStoreConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace PetStoreConsoleApp
@@ -8,7 +10,27 @@ namespace PetStoreConsoleApp
         static async Task Main()
         {
             var petService = new PetService();
-            var pets = await petService.GetAvailablePetsAsync();
+            Pet[] pets;
+            try
+            {
+                pets = await petService.GetAvailablePetsAsync();
+            }
+            catch (HttpRequestException)
+            {
+                Console.WriteLine("Pet store API service is down, please retry later");
+                return;
+            }
+            catch (JsonException)
+            {
+                Console.WriteLine("Result from Pet store API is unexpected, please retry later");
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong, please retry later");
+                return;
+            }
+
             var sortedPets = petService.SortPets(pets);
 
             Console.WriteLine("Display all pets");
